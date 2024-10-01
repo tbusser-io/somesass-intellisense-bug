e6bc373b91ceb6c66d96e5974ad8caa3f86e72d7
Thijs Busser <thijs.busser@iodigital.com>
2024-10-01 16:19:32 +0200

    Add demo files

---
+++ code/@apps/demo/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use 'tools/mixins' as mixins;
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  // TODO: When uncommenting the following line I would expect intellisense to
+  //       suggest the mixin "truncate" but instead intellisense doesn't suggest
+  //       anything at all.
+  //       When the setting "somesass.scss.completion.suggestFromUseOnly" is set
+  //       to false, intellisense does suggest the truncate mixin but it will
+  //       also suggest "error" but this mixin is not in this namespace.
+  // @include mixins.;
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
