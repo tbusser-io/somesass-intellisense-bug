52b083f9914a6f8ef53610e8b379be253aa6cec1
Thijs Busser <tbusser@gmail.com>
2024-10-10 15:19:53 +0200

    Rework shared styling project

--- code/@apps/demo/src/components/HelloWorld.vue
+++ code/@apps/demo/src/components/HelloWorld.vue
@@ -9,28 +9,36 @@ defineProps<{
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a class="foo" href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
   </div>
 </template>
 
 <style lang="scss" scoped>
-@use 'tools/mixins';
+@use '@workspace/shared-styles/abstracts/mixins' as mixins;
+@use '@workspace/shared-styles/abstracts/mixins/animation/' as animation;
+@use '@workspace/shared-styles/abstracts/tokens' as tokens;
+
+.foo {
+  @include animation.spin(3s);
+  font-family: tokens.$font-family-decorative;
+}
 
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
   position: relative;
   top: -10px;
+  @include mixins.button-loader;
 }
 
 h3 {
   // TODO: When uncommenting the following line I would expect intellisense to
   //       suggest the mixin "truncate" but instead intellisense doesn't suggest
   //       anything at all.
-  // @include mixins.;
   font-size: 1.2rem;
+  @include mixins.button-loader($speed: 2s);
 }
 
 .greetings h1,
--- code/@apps/demo/vite.config.ts
+++ code/@apps/demo/vite.config.ts
@@ -1,13 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { NodePackageImporter } from 'sass-embedded';
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Setting the api to modern-compiler removes the deprecation warning
+        // about the JS API.
+        api: 'modern-compiler',
+
+        // We need to add the NodePackageImporter so we can resolve the imports
+        // from the node_modules directory which are prefixed by "pkg:".
+        importers: [ new NodePackageImporter()]
+      }
+    }
+  },
+
   plugins: [
     vue(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
